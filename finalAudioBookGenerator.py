a42fabd9e7fe550f57a454df5888a819921f9054
globefire <sahun5774@gmail.com>
2019-07-05 03:05:17 +0530

    final script

--- finalAudioBookGenerator.py
+++ finalAudioBookGenerator.py
@@ -5,7 +5,7 @@ from pdf2image import convert_from_path
 filename = 'test.pdf'
  
 with tempfile.TemporaryDirectory() as path:
-     images_from_path = convert_from_path(filename, output_folder='F:/python projects cv tts stt/speechrecog', last_page=6, first_page =0)
+     images_from_path = convert_from_path(filename, output_folder='F:/python projects cv tts stt/speechrecog', last_page=2, first_page =0)
  
 base_filename  =  os.path.splitext(os.path.basename(filename))[0] + '.JPG'     
  
@@ -20,8 +20,32 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
 ######Tesseract ends HERE#########
 
+file = open("test.txt", "w")
 for page in images_from_path:
     name = os.path.splitext(os.path.basename(filename))[0] + str(i) +'.JPG'
     page.save(os.path.join(save_dir, name), 'JPEG')
     i+=1
-    print(pytesseract.image_to_string(Image.open(name)))
+    file.write(pytesseract.image_to_string(Image.open(name)))
+
+
+file.close()
+
+###Audio file generating###
+from gtts import gTTS
+file = open("test.txt", "r")
+txt = file.read()
+tts = gTTS(text = txt, lang = 'en')
+tts.save("read.mp3")
+file.close()
+
+
+
+
+
+
+
+
+
+
+
+##last edited 5/7/19 Developed By Nitin Sahu github.com/globefire
